a8bb3a127193c28cdbaf5e84200b35bae3507d3d
Sean Downey <sean@considerweb.com>
2016-11-22 22:24:40 +0000

    Allow for the selection of multiple options in "select_from_array" fields

--- src/resources/views/fields/select_from_array.blade.php
+++ src/resources/views/fields/select_from_array.blade.php
@@ -2,8 +2,9 @@
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
     <select
-        name="{{ $field['name'] }}"
+        name="{{ $field['name'] }}@if (isset($field['allows_multiple']) && $field['allows_multiple']==true)[]@endif"
         @include('crud::inc.field_attributes')
+        @if (isset($field['allows_multiple']) && $field['allows_multiple']==true)multiple @endif
     	>
 
         @if (isset($field['allows_null']) && $field['allows_null']==true)
@@ -13,7 +14,9 @@
 	    	@if (count($field['options']))
 	    		@foreach ($field['options'] as $key => $value)
 	    			<option value="{{ $key }}"
-                        @if ((isset($field['value']) && $key==$field['value']) || ( ! is_null( old($field['name']) ) && old($field['name']) == $key) )
+                        @if ((isset($field['value']) && $key==$field['value']) 
+                            || ( ! is_null( old($field['name']) ) && old($field['name']) == $key)
+                            || (is_array($field['value']) && in_array($key, $field['value'])) )
 							 selected
 						@endif
 	    			>{{ $value }}</option>
