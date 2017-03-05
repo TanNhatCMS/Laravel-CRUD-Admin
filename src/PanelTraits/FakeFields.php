2f98e790561f07291da31136fa6dad9449091b71
airways <isaac.accounts@mm.st>
2017-03-04 19:59:46 -0600

    Update FakeFields.php

--- src/PanelTraits/FakeFields.php
+++ src/PanelTraits/FakeFields.php
@@ -28,7 +28,7 @@ trait FakeFields
                 // add it to the request in its appropriate variable - the one defined, if defined
                 if (isset($fields[$k]['store_in'])) {
                     // json_decoded value is an object, not an array, so we need to cast it
-                    $request[$fields[$k]['store_in']] = (array)$request[$fields[$k]['store_in']];
+                    $request[$fields[$k]['store_in']] = (array) $request[$fields[$k]['store_in']];
                     $request[$fields[$k]['store_in']][$fields[$k]['name']] = $request[$fields[$k]['name']];
 
                     // remove the fake field
