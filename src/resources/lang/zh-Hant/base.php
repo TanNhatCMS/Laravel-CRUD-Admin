68c225e85493a38b1dae0f0e5fef8684686f1a24
yangshuai__ <962987003@qq.com>
2017-09-18 09:49:38 +0800

    add base chinese language

---
+++ src/resources/lang/zh-Hant/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => '注冊已經關閉.',
+    'first_page_you_see'   => '您看到的第一個頁面',
+    'login_status'         => '登陸狀態',
+    'logged_in'            => '您已經登陸!',
+    'toggle_navigation'    => '切換導航',
+    'administration'       => '管理',
+    'user'                 => '用戶',
+    'logout'               => '注銷',
+    'login'                => '登陸',
+    'register'             => '注冊',
+    'name'                 => '姓名',
+    'email_address'        => '郵箱地址',
+    'password'             => '密碼',
+    'confirm_password'     => '確認密碼',
+    'remember_me'          => '記住我',
+    'forgot_your_password' => '忘記密碼?',
+    'reset_password'       => '重置密码',
+    'send_reset_link'      => '發送重置密碼鏈接',
+    'click_here_to_reset'  => '點擊這裏重置你的密碼',
+    'unauthorized'         => '認證失敗。',
+    'dashboard'            => '儀表盤',
+    'handcrafted_by'       => '出自',
+    'powered_by'           => '使用',
+];
---
+++ src/resources/lang/zh-cn/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => '注册已经关闭.',
+    'first_page_you_see'   => '你看到的第一个页面',
+    'login_status'         => '登陆状态',
+    'logged_in'            => '您已登陆!',
+    'toggle_navigation'    => '切换导航',
+    'administration'       => '管理',
+    'user'                 => '用户',
+    'logout'               => '注销',
+    'login'                => '登陆',
+    'register'             => '注册',
+    'name'                 => '姓名',
+    'email_address'        => '邮箱地址',
+    'password'             => '密码',
+    'confirm_password'     => '确认密码',
+    'remember_me'          => '记住我',
+    'forgot_your_password' => '忘记密码?',
+    'reset_password'       => '重置密码',
+    'send_reset_link'      => '发送密码重置链接',
+    'click_here_to_reset'  => '点击这里去重置你的密码',
+    'unauthorized'         => '认证失败。',
+    'dashboard'            => '仪表盘',
+    'handcrafted_by'       => '出自',
+    'powered_by'           => '使用',
+];
