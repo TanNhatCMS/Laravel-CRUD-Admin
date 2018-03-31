d5923f092fb15341e40ed678ed994a876067b2c7
Ihsan Berahim <ihsanberahim@gmail.com>
2018-04-01 00:47:26 +0800

    minor fixes

--- src/resources/views/columns/text.blade.php
+++ src/resources/views/columns/text.blade.php
@@ -18,9 +18,9 @@
  $value = $entry->{$column_name};
 
  if(isset($column['translate']))
-  $text = trans("{$column['translate']}.{$column_name}");
+  $text = trans("{$column['translate']}.{$value}");
  else if(isset($column['translate_config']))
-  $text = config("{$column['translate_config']}.{$column_name}");
+  $text = config("{$column['translate_config']}.{$value}");
  else
   $text = (array_key_exists('prefix', $column) ? $column['prefix'] : '').str_limit(strip_tags($value), array_key_exists('limit', $column) ? $column['limit'] : 50, "[...]").(array_key_exists('suffix', $column) ? $column['suffix'] : '');
 
