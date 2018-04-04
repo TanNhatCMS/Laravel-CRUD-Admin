f8f92c3e85e0ca25f9a67b29f95508abbc33e0d4
Pavol Tanuska <pavol.tanuska@gmail.com>
2018-04-04 12:18:09 +0200

    Adding option to check/uncheck all fields in checklist

--- src/resources/views/fields/checklist.blade.php
+++ src/resources/views/fields/checklist.blade.php
@@ -1,6 +1,9 @@
-<!-- select2 -->
+<!-- checklist -->
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
+    @if (isset($field['batch']) && $field['batch'] === true)
+        <div class="select-all js-select-all">{{ trans('backpack::crud.select_all') }}</div>
+    @endif
     @include('crud::inc.field_translatable_icon')
     <?php $entity_model = $crud->getModel(); ?>
 
@@ -27,3 +30,48 @@
         <p class="help-block">{!! $field['hint'] !!}</p>
     @endif
 </div>
+
+{{-- ########################################## --}}
+{{-- Extra CSS and JS for this particular field --}}
+{{-- If a field type is shown multiple times on a form, the CSS and JS will only be loaded once --}}
+@if ($crud->checkIfFieldIsFirstOfItsType($field, $fields))
+
+    @push('crud_fields_styles')
+        <style>
+            .select-all {
+                cursor: pointer;
+                text-decoration: underline;
+            }
+        </style>
+    @endpush
+
+    {{-- FIELD JS - will be loaded in the after_scripts section --}}
+    @push('crud_fields_scripts')
+        <!-- include checklist js -->
+        <script>
+            jQuery(document).ready(function($) {
+
+                $('.js-select-all').bind('click', function(event) {
+                    event.preventDefault();
+                    var checkboxesContainer = $(this).next('.row');
+                    var checkboxes = checkboxesContainer.find('input[type="checkbox"]');
+
+                    // Treat everything as unchecked by default (first action will be checking)
+                    var checked = false;
+
+                    // But if everything is checked, first action is unchecking
+                    if (checkboxesContainer.find('input[type="checkbox"]:checked').length == checkboxes.length) {
+                        checked = true;
+                    }
+
+                    checkboxes.each(function() {
+                        $(this).prop('checked', !checked);
+                    })
+                });
+            });
+        </script>
+    @endpush
+
+@endif
+{{-- End of Extra CSS and JS --}}
+{{-- ########################################## --}}
