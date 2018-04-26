f5c93518eb6ba01e7f52841fc78799ea65663e20
Muhammad Nur'Ihsan Bin Berahim <ihsanberahim@gmail.com>
2018-04-26 19:20:20 +0800

    Make save options optional
    Sometime, really no need options.

--- src/resources/views/inc/form_save_buttons.blade.php
+++ src/resources/views/inc/form_save_buttons.blade.php
@@ -8,18 +8,18 @@
             <span class="fa fa-save" role="presentation" aria-hidden="true"></span> &nbsp;
             <span data-value="{{ $saveAction['active']['value'] }}">{{ $saveAction['active']['label'] }}</span>
         </button>
+        @if(isset($saveAction['options']) && !empty($saveAction['options']))
+            <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aira-expanded="false">
+                <span class="caret"></span>
+                <span class="sr-only">&#x25BC;</span>
+            </button>
 
-        <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aira-expanded="false">
-            <span class="caret"></span>
-            <span class="sr-only">&#x25BC;</span>
-        </button>
-
-        <ul class="dropdown-menu">
-            @foreach( $saveAction['options'] as $value => $label)
-            <li><a href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a></li>
-            @endforeach
-        </ul>
-
+            <ul class="dropdown-menu">
+                @foreach( $saveAction['options'] as $value => $label)
+                    <li><a href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a></li>
+                @endforeach
+            </ul>
+        @endif
     </div>
 
     <a href="{{ url($crud->route) }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
