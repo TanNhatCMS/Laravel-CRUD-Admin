47bdd2b68b1a1ee4de6f404fb4bcad0256876b01
NewEXE <v.voloshyn96@gmail.com>
2018-09-07 01:49:05 +0300

    Added types map (for mapping backpack column types to editable types); Added ability to add custom title for (editable 'name' option).

--- src/resources/views/editable.blade.php
+++ src/resources/views/editable.blade.php
@@ -1,24 +1,44 @@
 @if(config('backpack.base.enable_editable') && !empty($column['editable']))
-
     @if(is_array($column['editable']))
         @php
-            // map types
-            // performs options check
+            if (isset($column['type'])) {
+                /*
+                * Backpack column type => Editable type
+                *
+                * Only one type is supported for now.
+                */
+                $typesMap = [
+                    'text' => 'text',
+                ];
+
+                if (in_array($column['type'], array_keys($typesMap))) {
+                    $column['editable']['type'] = $typesMap[$column['type']];
+                }
+            }
         @endphp
     @endif
+
+    @if(!isset($column['editable']['type']))
+        @php
+            $column['editable']['type'] = 'text'; // fallback type
+        @endphp
+    @endif
+    @if(isset($column['editable']['mode']))
+        @php
+            $column['editable']['mode'] = in_array($column['editable']['mode'], ['popup', 'inline']) ?
+                        $column['editable']['mode'] : null;
+        @endphp
+    @endif
+
     <!-- editable for column [{{ $column['name'] }}] -->
     <script>
         $(document).ready(function() {
-            //toggle `popup` / `inline` mode
-            $.fn.editable.defaults.mode = 'inline'; //set via config
-
             $('#{{ $column['name'] . '-' . $entry->getKey() }}').editable({
-                {!! !empty($column['editable']['mode']) ? 'mode: ' . '"'.$column['editable']['mode'].'",' : '' !!}
-                title:  'Enter {{ $column['name'] }}',
+                {!! !empty($column['editable']['mode']) ? "mode: '{$column['editable']['mode']}'," : '' !!}
+                title:  '{{ $column['editable']['title'] ?? "Enter {$column['editable']['title']}" }}',
                 name:   '{{ $column['name'] }}',
-                type:   '{{ $column['editable']['type'] ?? 'text' }}',
+                type:   '{{ $column['editable']['type'] }}',
                 pk:     '{{ $entry->getKey() }}',
-
                 params: {
                     '_editable': true,
                 },
@@ -26,7 +46,7 @@
                     type: 'POST',
                     dataType: 'json'
                 },
-                url: '{{ url( trim($crud->getRoute(), '/') . '/editable' ) }}'
+                url: '{{ url(trim($crud->getRoute(), '/') . '/editable' ) }}'
             });
         });
     </script>
