1b091daacbdf4778d1301f92b9a988d86b937636
flartet <florent.lartet@univ-tlse2.fr>
2019-02-12 11:30:01 +0100

    add limit option usage on select column
    Like the text field, the idea is to be able to shorten this column if the content is too long, with the help of 'limit' => xx .

--- src/resources/views/columns/select.blade.php
+++ src/resources/views/columns/select.blade.php
@@ -3,7 +3,12 @@
     <?php
         $attributes = $crud->getModelAttributeFromRelation($entry, $column['entity'], $column['attribute']);
         if (count($attributes)) {
-            echo e(implode(', ', $attributes));
+            if (isset($column['limit'])) {
+                echo e(substr(implode(', ', $attributes), 0, $column['limit'])).'[&hellip;]';
+            }
+            else {
+                echo e(implode(', ', $attributes));
+            }
         } else {
             echo '-';
         }
