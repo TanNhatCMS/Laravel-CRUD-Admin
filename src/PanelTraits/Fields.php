f273a8f22adb0f97321c7388193f9ead94813641
Wesley Smith <wesley.smith@bookit.com>
2019-04-13 21:58:20 -0500

    Added shortcut for setting form a field applies to

--- src/PanelTraits/Fields.php
+++ src/PanelTraits/Fields.php
@@ -18,6 +18,9 @@ trait Fields
      */
     public function addField($field, $form = 'both')
     {
+        if (!empty($field['form'])) {
+            $form = $field['form'];
+        }
         // if the field_definition_array array is a string, it means the programmer was lazy and has only passed the name
         // set some default values, so the field will still work
         if (is_string($field)) {
