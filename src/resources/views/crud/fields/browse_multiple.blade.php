cc1493841b640a1d1b4fe142c08eb2d74773261e
Bogdan Lotarev <bogdanlotar@gmail.com>
2020-01-09 13:36:08 +0300

    Fix elfinder script include in browse_multiple.blade.php for non En locale

--- src/resources/views/crud/fields/browse_multiple.blade.php
+++ src/resources/views/crud/fields/browse_multiple.blade.php
@@ -116,8 +116,7 @@ if (!isset($field['wrapperAttributes']) || !isset($field['wrapperAttributes']['d
         <script type="text/javascript" src="{{ asset('packages/barryvdh/elfinder/js/elfinder.min.js') }}"></script>
         {{-- <script type="text/javascript" src="{{ asset('packages/barryvdh/elfinder/js/extras/editors.default.min.js') }}"></script> --}}
         @if ( ($locale = \App::getLocale()) != 'en' )
-            @php
-            <script type="text/javascript" src="{{ asset("packages/barryvdh/elfinder/js/i18n/elfinder.{$locale}.js") }}"></script>
+            <script type="text/javascript" src="{{ asset(sprintf('packages/barryvdh/elfinder/js/i18n/elfinder.%s.js', $locale)) }}"></script>
         @endif
 
         <script>
