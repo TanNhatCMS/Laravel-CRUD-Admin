26e1cc332c0f5c14b87c1ceec068724c50231968
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-03-23 19:24:43 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/app/Charts/ChartSkeleton.php
+++ src/app/Charts/ChartSkeleton.php
@@ -6,12 +6,12 @@ use ConsoleTVs\Charts\Classes\Chartjs\Chart as ChartJs;
 
 class ChartSkeleton extends ChartJs
 {
-	public function __construct()
-	{
-		// construct the Chart using standard LaravelCharts
+    public function __construct()
+    {
+        // construct the Chart using standard LaravelCharts
         parent::__construct();
 
         // call the setup method (where we instruct users to configure their charts)
-		$this->setup();
-	}
+        $this->setup();
+    }
 }
