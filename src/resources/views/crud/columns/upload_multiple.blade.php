1edb046f00f7965cbf180545d25fc6fb6b05d8a9
VREEdom <35516476+vonsogt@users.noreply.github.com>
2020-03-24 13:26:28 +0700

    patch upload_multiple.blade.php
    fix bug undefined variable

--- src/resources/views/crud/columns/upload_multiple.blade.php
+++ src/resources/views/crud/columns/upload_multiple.blade.php
@@ -10,8 +10,8 @@
     @if ($value && count($value))
         @foreach ($value as $file_path)
         @php
-            $column['wrapper']['href'] = $column['wrapper']['href'] ?? isset($column['disk'])?asset(\Storage::disk($column['disk'])->url($file_path)):asset($prefix.$file_path);
-            $text = $prefix.$file_path;
+            $column['wrapper']['href'] = $column['wrapper']['href'] ?? isset($column['disk'])?asset(\Storage::disk($column['disk'])->url($file_path)):asset($column['prefix'].$file_path);
+            $text = $column['prefix'].$file_path;
         @endphp
             @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
             @if($column['escaped'])
