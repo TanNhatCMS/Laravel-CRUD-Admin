900367c9aec1cf19d2424b139645a2d8cfde7fe9
Abby <me@abbyjanke.com>
2020-03-29 01:47:52 -0700

    counting select_multiple records

--- src/resources/views/crud/columns/select_multiple.blade.php
+++ src/resources/views/crud/columns/select_multiple.blade.php
@@ -5,7 +5,9 @@
 
 <span>
     <?php
-        if ($results && $results->count()) {
+    	if($column['count']) {
+    		echo $results->count();
+    	} elseif ($results && $results->count()) {
             $results_array = $results->pluck($column['attribute']);
             echo implode(', ', $results_array->toArray());
         } else {
