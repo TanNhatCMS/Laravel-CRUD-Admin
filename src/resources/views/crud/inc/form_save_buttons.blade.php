ee8f2c1a6eaae9f6ec43f2d432bfec3e9ae4d4d1
Adli I. Ifkar <adly.shadowbane@gmail.com>
2020-03-31 21:08:42 +0700

    Simple patch to use the setting value showSaveActionChange

--- src/resources/views/crud/inc/form_save_buttons.blade.php
+++ src/resources/views/crud/inc/form_save_buttons.blade.php
@@ -9,16 +9,21 @@
             <span data-value="{{ $saveAction['active']['value'] }}">{{ $saveAction['active']['label'] }}</span>
         </button>
 
-        <div class="btn-group" role="group">
-            <button id="btnGroupDrop1" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span><span class="sr-only">&#x25BC;</span></button>
-            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                @foreach( $saveAction['options'] as $value => $label)
-                <a class="dropdown-item" href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a>
-                @endforeach
+        @if (config('backpack.crud.operations.' . $crud->getCurrentOperation() . '.showSaveActionChange'))
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"><span class="caret"></span><span
+                        class="sr-only">&#x25BC;</span></button>
+                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                    @foreach( $saveAction['options'] as $value => $label)
+                        <a class="dropdown-item" href="javascript:void(0);" data-value="{{ $value }}">{{ $label }}</a>
+                    @endforeach
+                </div>
             </div>
-          </div>
+        @endif
 
     </div>
 
-    <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}" class="btn btn-default"><span class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
+    <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}" class="btn btn-default"><span
+            class="fa fa-ban"></span> &nbsp;{{ trans('backpack::crud.cancel') }}</a>
 </div>
