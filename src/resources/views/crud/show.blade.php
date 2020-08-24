3889405fa4b37c0e7fcab560bcae920b246151c9
Martijn van der Burg <martijn.vanderburg@gmail.com>
2020-08-24 16:28:32 +0200

    header classes config

--- src/resources/views/crud/show.blade.php
+++ src/resources/views/crud/show.blade.php
@@ -13,14 +13,18 @@
 
 @section('header')
 	<section class="container-fluid d-print-none">
-    	<a href="javascript: window.print();" class="btn float-right"><i class="la la-print"></i></a>
-		<h2>
-	        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-	        <small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
-	        @if ($crud->hasAccess('list'))
-	          <small class=""><a href="{{ url($crud->route) }}" class="font-sm"><i class="la la-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
-	        @endif
-	    </h2>
+		<div class="row">
+			<div class="{{ $crud->getShowHeaderClass() }}">
+				<a href="javascript: window.print();" class="btn float-right"><i class="la la-print"></i></a>
+				<h2>
+					<span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+					<small>{!! $crud->getSubheading() ?? mb_ucfirst(trans('backpack::crud.preview')).' '.$crud->entity_name !!}.</small>
+					@if ($crud->hasAccess('list'))
+					<small class=""><a href="{{ url($crud->route) }}" class="font-sm"><i class="la la-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a></small>
+					@endif
+				</h2>
+			</div>
+		</div>
     </section>
 @endsection
 
