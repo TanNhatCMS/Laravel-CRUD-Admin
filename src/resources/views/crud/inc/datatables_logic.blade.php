49668376d2d927f797c6536fbbd856f150ff3d17
Pedro X <pxpm88@gmail.com>
2020-09-03 12:23:55 +0100

    css fixes

--- src/resources/views/crud/inc/datatables_logic.blade.php
+++ src/resources/views/crud/inc/datatables_logic.blade.php
@@ -211,7 +211,7 @@
           dom:
             "<'row hidden'<'col-sm-6 hidden-xs'i><'col-sm-6 hidden-print'f>>" +
             "<'row'<'col-sm-12'tr>>" +
-            "<'row mt-2 '<'col-sm-6 col-md-4'l><'col-sm-2 col-md-4 text-center'B><'col-sm-6 col-md-4 hidden-print'p>>",
+            "<'row mt-2 '<'col-sm-6 col-md-4'l><'col-sm-2 col-md-4 text-center'B><'col-sm-6 col-md-4 pagination_buttons hidden-print'p>>",
       }
   }
   </script>
