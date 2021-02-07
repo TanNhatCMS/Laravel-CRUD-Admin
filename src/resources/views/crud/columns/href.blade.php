111a1559b6e8de83c4cd1bc1f45a67b6d3f8e19d
António Almeida <promatik@gmail.com>
2021-02-07 23:33:05 +0000

    Update src/resources/views/crud/columns/href.blade.php

--- src/resources/views/crud/columns/href.blade.php
+++ src/resources/views/crud/columns/href.blade.php
@@ -3,7 +3,7 @@
     $column['text'] = $column['text'] ?? $value;
 @endphp
 <span>
-	@includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
+    @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
     <a href=" {!! $value !!}">{{$column['text']}}</a>
     @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_end')
 </span>
