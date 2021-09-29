d204b8ad35445da9197ce6480929f7f5d8eb0431
AbdulRahman El-zahaby <el3zahaby@gmail.com>
2021-09-29 03:04:39 +0200

    Update upload_multiple.blade.php

--- src/resources/views/crud/columns/upload_multiple.blade.php
+++ src/resources/views/crud/columns/upload_multiple.blade.php
@@ -1,5 +1,6 @@
 @php
     $value = data_get($entry, $column['name']);
+    if (is_string($value)) $value = json_decode($value); 
     $column['prefix'] = $column['prefix'] ?? '';
     $column['disk'] = $column['disk'] ?? null;
     $column['escaped'] = $column['escaped'] ?? true;
