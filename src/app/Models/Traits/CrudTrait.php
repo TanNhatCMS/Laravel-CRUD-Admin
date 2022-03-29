c0fcb2ddc5be6701e31bfc67256c554096e7feb1
Selim GORMUS <siberfx@users.noreply.github.com>
2022-03-29 04:25:00 +0300

    trashedfilter scope and model table getter
    trashed filter scope and getter for models table name added

--- src/app/Models/Traits/CrudTrait.php
+++ src/app/Models/Traits/CrudTrait.php
@@ -2,6 +2,8 @@
 
 namespace Backpack\CRUD\app\Models\Traits;
 
+use Illuminate\Support\Facades\Schema;
+
 trait CrudTrait
 {
     use HasIdentifiableAttribute;
@@ -14,5 +16,25 @@ trait CrudTrait
     public static function hasCrudTrait()
     {
         return true;
+    }
+    
+    /**
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return with(new static)->getTable();
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public static function scopeWithTrashedFiltered($query)
+    {
+        if (Schema::hasColumn(self::getTableName(), 'deleted_at')) {
+            $query->withTrashed();
+        }
+        return $query;
     }
 }
