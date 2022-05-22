ed6dbcb1534b0d378edbf4b0b84f605ec2f80c46
Antonio Almeida <promatik@gmail.com>
2022-05-22 23:03:40 +0100

    Replaced jQuery with Vanilla JS

--- src/resources/views/crud/inc/form_fields_script.blade.php
+++ src/resources/views/crud/inc/form_fields_script.blade.php
@@ -7,20 +7,21 @@
      * too, by exposing the main components (name, wrapper, input).
      */
     class CrudField {
-        constructor(fieldName) {
-            this.name = fieldName;
-            this.wrapper = $(`[bp-field-name="${this.name}"]`);
-            this.input = this.wrapper.closest('[bp-field-main-input]').first();
-
+        constructor(name) {
+            this.name = name;
+            this.wrapper = document.querySelector(`[bp-field-name="${this.name}"]`);
             // if no bp-field-main-input has been declared in the field itself,
             // assume it's the first input in that wrapper, whatever it is
-            if (this.input.length === 0) {
-                this.input = this.wrapper.find('input, textarea, select').first();
+            this.input = this.wrapper?.querySelector('[bp-field-main-input], input, textarea, select');
+
+            // Validate that the field has been found
+            if(!this.wrapper || !this.input) {
+                console.error(`No wrapper or input found for the field ${this.name}`);
             }
         }
 
         get value() {
-            let value = this.input.val();
+            let value = this.input.value;
 
             // Parse the value if it's a number
             if (value.length && !isNaN(value)) {
@@ -33,15 +34,22 @@
         change(closure) {
             const fieldChanged = event => {
                 const wrapper = this.input.closest('[bp-field-wrapper=true]');
-                const name = wrapper.attr('bp-field-name');
-                const type = wrapper.attr('bp-field-type');
-                const value = this.input.val();
+                const name = wrapper.getAttribute('bp-field-name');
+                const type = wrapper.getAttribute('bp-field-type');
+                const value = this.value;
 
                 closure(event, value, name, type);
             };
 
-            this.input[0]?.addEventListener('input', fieldChanged, false);
-            $(this.input).change(fieldChanged);
+            // Change event Listeners
+            new MutationObserver(fieldChanged).observe(this.input, { attributes: true });
+            this.input.addEventListener('change', fieldChanged, false);
+
+            // Detect input changes only on inputs and textareas
+            if(['INPUT', 'TEXTAREA'].includes(this.input.nodeName)) {
+                this.input.addEventListener('input', fieldChanged, false);
+            }
+
             fieldChanged();
 
             return this;
@@ -52,8 +60,9 @@
         }
 
         show(value = true) {
-            this.wrapper.toggleClass('d-none', !value);
-            this.input.trigger(`backpack:field.${value ? 'show' : 'hide'}`);
+            this.wrapper.classList.toggle('d-none', !value);
+            this.input.dispatchEvent(new CustomEvent(`backpack:field.${value ? 'show' : 'hide'}`, { bubbles: true }));
+            // $(this.input).trigger(`backpack:field.${value ? 'show' : 'hide'}`);
             return this;
         }
 
@@ -62,8 +71,13 @@
         }
 
         enable(value = true) {
-            this.input.attr('disabled', !value && 'disabled');
-            this.input.trigger(`backpack:field.${value ? 'enable' : 'disable'}`);
+            if(value) {
+                this.input.removeAttribute('disabled');
+            } else {
+                this.input.setAttribute('disabled', 'disabled');
+            }
+            this.input.dispatchEvent(new CustomEvent(`backpack:field.${value ? 'enable' : 'disable'}`, { bubbles: true }));
+            // $(this.input).trigger(`backpack:field.${value ? 'enable' : 'disable'}`);
             return this;
         }
 
@@ -72,8 +86,9 @@
         }
 
         require(value = true) {
-            this.wrapper.toggleClass('required', value);
-            this.input.trigger(`backpack:field.${value ? 'require' : 'unrequire'}`);
+            this.wrapper.classList.toggle('required', value);
+            this.input.dispatchEvent(new CustomEvent(`backpack:field.${value ? 'require' : 'unrequire'}`, { bubbles: true }));
+            // $(this.input).trigger(`backpack:field.${value ? 'require' : 'unrequire'}`);
             return this;
         }
 
@@ -82,7 +97,9 @@
         }
 
         check(value = true) {
-            this.wrapper.find('input[type=checkbox]').prop('checked', value).trigger('change');
+            let checkbox = this.wrapper.querySelector('input[type=checkbox]');
+            checkbox.checked = value;
+            checkbox.dispatchEvent(new Event('change'));
             return this;
         }
 
