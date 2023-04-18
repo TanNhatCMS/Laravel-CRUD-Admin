c2b0bb22dcf32c179a95a126203cd929f5a81086
Niko <nikolaistehl@werbewolke.de>
2023-04-18 16:54:21 +0200

    add nl2br option for textarea column

--- src/resources/views/crud/columns/textarea.blade.php
+++ src/resources/views/crud/columns/textarea.blade.php
@@ -5,13 +5,14 @@
     $column['prefix'] = $column['prefix'] ?? '';
     $column['suffix'] = $column['suffix'] ?? '';
     $column['text'] = $column['default'] ?? '-';
+    $column['nl2br'] = $column['nl2br'] ?? false;
 
     if($column['value'] instanceof \Closure) {
         $column['value'] = $column['value']($entry);
     }
 
     if(!empty($column['value'])) {
-        $column['text'] = $column['prefix'].$column['value'].$column['suffix'];
+        $column['text'] = $column['prefix'].$column['nl2br'] ? nl2br($column['value']) : $column['value'].$column['suffix'];
     }
 @endphp
 
